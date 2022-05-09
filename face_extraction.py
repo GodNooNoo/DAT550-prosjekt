5b979679a58eeba821142be192ec9ebf8cf9d45f
Dag Hermann Valvik <31191858+DagValvik@users.noreply.github.com>
2022-05-09 16:56:33 +0200

    new pictures

--- face_extraction.py
+++ face_extraction.py
@@ -1,94 +1,73 @@
-import sys
-import cv2 as cv
 import os
+import sys
+
+import cv2 as cv
+import cv2
 import face_recognition
+import pandas as pd
+from tqdm import tqdm
 
-PATH_VIDS = ''
-PATH_FRAMES = ''
-PATH_FACES = ''
+PATH_VIDS = ""
+PATH_REAL = ""
+PATH_FAKE = ""
 
 
-def video_to_frames():
+def extract_faces_optimized():
     """
-    This function splits the videos into frames and saves the frames in the frames folder.
-    Returns:
-        int: Count of videos converted to frames.
+    This function extracts the faces from the frames in the videos and saves it to the real or fake folder.
     """
+    METADATA = pd.read_json("sample_set/metadata.json")
 
-    if not os.path.isdir(PATH_FRAMES):
-        os.mkdir(PATH_FRAMES)
+    if not os.path.isdir(PATH_REAL):
+        os.mkdir(PATH_REAL)
+    if not os.path.isdir(PATH_FAKE):
+        os.mkdir(PATH_FAKE)
 
-    videos = [os.path.join(PATH_VIDS, vid) for vid in os.listdir(
-        PATH_VIDS) if vid.endswith('.mp4')]
-    for video in videos:
+    videos = [
+        os.path.join(PATH_VIDS, vid)
+        for vid in os.listdir(PATH_VIDS)
+        if vid.endswith(".mp4")
+    ]
+
+    for video in tqdm(videos):
+        if METADATA[os.path.basename(video)]["label"] == "REAL":
+            folder = PATH_REAL
+        else:
+            folder = PATH_FAKE
         # Create a folder for each video
-        frames_folder = os.path.join(
-            PATH_FRAMES, os.path.basename(video)[:-4])
-        if not os.path.isdir(frames_folder):
-            os.mkdir(frames_folder)
+        folder = os.path.join(folder, os.path.basename(video))
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
         # Extract the frames
         vid = cv.VideoCapture(video)
-        count = 0
+        frames = []
         while True:
-            count += 1
             success, image = vid.read()
             if not success:
                 break
-            cv.imwrite(os.path.join(frames_folder, f"frame{count}.jpg"), image)
-    return len(videos)
-
-
-def extract_faces():
-    """
-    This function extracts the faces from the frames in the frames folder and saves the faces in the faces folder.
-    """
-
-    if not os.path.isdir(PATH_FACES):
-        os.mkdir(PATH_FACES)
-
-    # Get all the folders in the frames_folder
-    folders = [os.path.join(PATH_FRAMES, f) for f in os.listdir(
-        PATH_FRAMES) if os.path.isdir(os.path.join(PATH_FRAMES, f))]
-    for folder in folders:
-        # Create a folder for each video
-        faces_folder = os.path.join(
-            PATH_FACES, os.path.basename(folder))
-        if not os.path.isdir(faces_folder):
-            os.mkdir(faces_folder)
-        # Get all the frames in the folder
-        frames = [os.path.join(folder, f)
-                    for f in os.listdir(folder) if f.endswith('.jpg')]
-        for frame in frames:
-            # Get the image
-            img = face_recognition.load_image_file(frame)
-            # Get the face locations
-            face_locations = face_recognition.face_locations(img)
+            image = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
+            frames.append(image)
+        for frame in tqdm(frames):
+            # extract the faces
+            faces = face_recognition.face_locations(frame)
             # For each face in the image
-            face_count = 0
-            for face_location in face_locations:
-                face_count += 1
+            for i, face in enumerate(faces):
                 # Get the top, right, bottom, left
-                top, right, bottom, left = face_location
-                # Crop the face
-                face = img[top:bottom, left:right]
+                top, right, bottom, left = face
+                # Crop the face with padding of 30 pixels
+                face = frame[top - 30 : bottom + 30, left - 30 : right + 30]
                 # Save the face
-                cv.imwrite(os.path.join(faces_folder, os.path.basename(
-                    frame)[:-4] + f"_face{face_count}.jpg"), face)
-    
+                cv.imwrite(os.path.join(folder, f"face{i}.jpg"), face)
+        vid.release()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python face_extraction.py <path_to_videos_folder>")
         sys.exit(1)
 
     PATH_VIDS = sys.argv[1]
-    PATH_FRAMES = os.path.join(PATH_VIDS, "frames")
-    PATH_FACES = os.path.join(PATH_VIDS, "faces")
+    PATH_REAL = os.path.join(PATH_VIDS, "real")
+    PATH_FAKE = os.path.join(PATH_VIDS, "fake")
 
-    # Extract the frames
-    num_vids = video_to_frames()
-    print("Extracted frames from {} videos".format(num_vids))
-
-    # Extract the faces
-    extract_faces()
-    print("Extracted faces")
+    extract_faces_optimized()
